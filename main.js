eef9846062c84d567deededcf94e15d854ea5705
Jalal KADDOURI <jalalkadori2@gmail.com>
2022-12-28 06:15:02 +0000

    Adding script and deleting css file

--- main.js
+++ main.js
@@ -80,55 +80,115 @@ function Add(){
         console.log(ProduitData);
     }
 
-    let afficherBtn = document.getElementById('Afficher');
+    
+    clear();
+}
 
-    afficherBtn.onclick = function show() {
-        let table = document.getElementById('table');
+function show() {
+    let table = document.getElementById('table');
 
-        for(let j=0;j<ProduitData.length;j++){
+    for(let j=0;j<ProduitData.length;j++){
 
-            let row = document.createElement('tr');
+        let row = document.createElement('tr');
 
-            for(let i=0;i< 7; i++) {
-                let cell = document.createElement('td');
-                let SuppBtn = document.createElement('button');
-                let UppdateBtn = document.createElement('button');
-                row.appendChild(cell);
-                if(i==0) {
-                    cell.innerHTML = ProduitData[j].Nom;
-                }
-                if(i==1) {
-                    cell.innerHTML = ProduitData[j].Marque;
-                }
-                if(i==2) {
-                    cell.innerHTML = ProduitData[j].Prix;
-                }
-                if(i==3) {
-                    cell.innerHTML = ProduitData[j].Date;
-                }
-                if(i==4) {
-                    cell.innerHTML = ProduitData[j].type;
-                }
-                if(i==5) {
-                    cell.innerHTML = ProduitData[j].Promotion;
-                }
-                if(i==6) {
-                    // iserer un botton de modification  
-                    UppdateBtn.innerText = "Modifier";
-                    UppdateBtn.setAttribute("class", "btn btn-info mx-2");
-                    // UppdateBtn.setAttribute("onclick", "modification(this)");
-                    cell.appendChild(UppdateBtn);
-                    // iserer un botton de suppression 
-                    SuppBtn.innerText = "Supprimer";
-                    SuppBtn.setAttribute("class", "btn btn-danger");
-                    SuppBtn.setAttribute("onclick", "deletRow(this)");
-                    cell.appendChild(SuppBtn);
-                }
-            table.appendChild(row);
+        for(let i=0;i< 7; i++) {
+            let cell = document.createElement('td');
+            let SuppBtn = document.createElement('button');
+            let UppdateBtn = document.createElement('button');
+            row.appendChild(cell);
+            if(i==0) {
+                cell.innerHTML = ProduitData[j].Nom;
             }
+            if(i==1) {
+                cell.innerHTML = ProduitData[j].Marque;
+            }
+            if(i==2) {
+                cell.innerHTML = ProduitData[j].Prix;
+            }
+            if(i==3) {
+                cell.innerHTML = ProduitData[j].Date;
+            }
+            if(i==4) {
+                cell.innerHTML = ProduitData[j].type;
+            }
+            if(i==5) {
+                cell.innerHTML = ProduitData[j].Promotion;
+            }
+            if(i==6) {
+                // iserer un botton de modification  
+                UppdateBtn.innerText = "Modifier";
+                UppdateBtn.setAttribute("class", "btn btn-info mx-2");
+                UppdateBtn.setAttribute("onclick", "modification(this)");
+                cell.appendChild(UppdateBtn);
+                // iserer un botton de suppression 
+                SuppBtn.innerText = "Supprimer";
+                SuppBtn.setAttribute("class", "btn btn-danger");
+                SuppBtn.setAttribute("onclick", "deletRow(this)");
+                cell.appendChild(SuppBtn);
+            }
+        table.appendChild(row);
         }
     }
-    clear();
+}
+
+
+function deletRow(r) {
+    let indexOfRow = (r.parentNode.parentNode.rowIndex) - 1;
+    let table = document.getElementById('table');
+    table.deleteRow(indexOfRow);
+    console.log(indexOfRow);
+    
+}
+
+// Modification function 
+function modification(x) {
+    let table = document.getElementById('table');
+    let Nom = document.getElementById('Nom');
+    let Marque = document.getElementById('Marque');
+    let Prix = document.getElementById('Prix');
+    let Date = document.getElementById('Date');
+    let type = document.getElementById('type');
+    let Promo = document.getElementById('Oui');
+    let noPromo = document.getElementById('Non');
+    let addButton = document.getElementById('Ajouter');
+    let ModiButton = document.getElementById('Modifier');
+    
+    addButton.setAttribute("class", "d-none");
+    ModiButton.setAttribute("class", "btn btn-info d-inline-block");
+    var i = (x.parentNode.parentNode.rowIndex) - 1;
+
+    console.log(i);
+
+    Nom.value = table.rows[i].cells[0].innerText;
+    Marque.value = table.rows[i].cells[1].innerText;
+    Prix.value = table.rows[i].cells[2].innerText;
+    Date.value = table.rows[i].cells[3].innerText;
+    type.value = table.rows[i].cells[4].innerText;
+    const radio = table.rows[i].cells[5].innerHTML;
+    if ( radio == "Oui") {
+        Promo.checked = true; 
+    } else {
+        noPromo.checked = true; 
+    }
+
+    ModiButton.onclick = function Update() {
+
+        let getSelectedValue = document.querySelector( 'input[name="Promotion"]:checked');
+        let Promotion = getSelectedValue.value;
+        addButton.setAttribute("class", "btn btn-primary d-inline-block");
+        ModiButton.setAttribute("class","d-none");
+
+        table.rows[i].cells[0].innerText = Nom.value;
+        table.rows[i].cells[1].innerText = Marque.value;
+        table.rows[i].cells[2].innerText = Prix.value;
+        table.rows[i].cells[3].innerText = Date.value;
+        table.rows[i].cells[4].innerText = type.value;
+        table.rows[i].cells[5].innerText = Promotion;
+
+        clear();
+    }
+    
+    
 }
 
 function clear(){
@@ -141,53 +201,4 @@ function clear(){
     document.getElementById('Non').checked = false;
 }
 
-function deletRow(r) {
-    let indexOfRow = r.parentNode.parentNode.rowIndex;
-    let table = document.getElementById('table');
-    table.deleteRow(indexOfRow);
-}
-
-// Modification function 
-function modification(x) {
-    let Nom = document.getElementById('Nom');
-    let Marque = document.getElementById('Marque');
-    let Prix = document.getElementById('Prix');
-    let Date = document.getElementById('Date');
-    let type = document.getElementById('type');
-    let Promo = document.getElementById('Oui');
-    let noPromo = document.getElementById('Non');
-    const addButton = document.getElementById('Ajouter');
-    
-
-    addButton.style.display = "none";
-    ModiButton.style.display = "block"; 
-    var i = x.parentNode.parentNode.rowIndex;
-
-    ModiButton.onclick = function Update() {
-        table.rows[i].cells[0].innerText = Nom.value;
-        table.rows[i].cells[1].innerText = Marque.value;
-        table.rows[i].cells[2].innerText = Prix.value;
-        table.rows[i].cells[3].innerText = Date.value;
-        table.rows[i].cells[4].innerText = type.value;
-
-        let getSelectedValue = document.querySelector('input[name="promotion"]:checked');
-        table.rows[i].cells[5].innerText = getSelectedValue.value;
-
-        addButton.style.display = "block";
-        ModiButton.style.display = "none";
-        clear()
-    }
-    
-    Nom.value = table.rows[i].cells[0].innerText;
-    Marque.value = table.rows[i].cells[1].innerText;
-    Prix.value = table.rows[i].cells[2].innerText;
-    Date.value = table.rows[i].cells[3].innerText;
-    Type.value = table.rows[i].cells[4].innerText;
-    const radio = table.rows[i].cells[5].innerHTML;
-    if ( radio == "Oui") {
-        Promo.checked = true; 
-    } else {
-        noPromo.checked = true; 
-    }
-}
 
